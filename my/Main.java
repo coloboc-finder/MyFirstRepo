d594e5c4c56bacc459c71358c387fb92cd6c58ef
coloboc-finder <gk120303@gmail.com>
2024-12-02 01:22:53 +0500

    "задача 1.1"

--- my/Main.java
+++ my/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         if (a > b) System.out.println("a > b. a равно = " + a);
         if (a < b)
-            for (count = 1; count <= 5; count = count+1)
+            for (count = 1; count <= 6; count = count+1)
                 System.out.println("a < b. (a + b) * +1 = " + (a + b) * count);
         System.out.println("DONE");
 

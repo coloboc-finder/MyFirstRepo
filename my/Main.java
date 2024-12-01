5c2b78fb7dcca9b24eb2d099618af723da57a405
coloboc-finder <gk120303@gmail.com>
2024-12-02 01:21:12 +0500

    "задача 1"

---
+++ my/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double a, b;
+
+        a = 1;
+        b = 11;
+
+        double count;
+
+        if (a > b) System.out.println("a > b. a равно = " + a);
+        if (a < b)
+            for (count = 1; count <= 5; count = count+1)
+                System.out.println("a < b. (a + b) * +1 = " + (a + b) * count);
+        System.out.println("DONE");
+
+    }
+}
